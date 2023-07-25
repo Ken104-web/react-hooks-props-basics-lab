6079892a6e22ea09664aa11243df56ecb86db8df
KenCorp <kennethwarui35@gmail.com>
2023-07-25 15:19:04 +0300

    Test done

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,14 @@
 import React from "react";
-
-function About({bio}) {
+import Links from "./Link";
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{bio}</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
 
 export default About;
-
